82a07576762ab905619a0f998f09e00552232fa4
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2025-06-09 08:57:34 +0800

    Add global input feature to CoreML model builder
    - Added input for global features to the model builder.

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -5,16 +5,25 @@
 
 int main()
 {
+    KataGoCoreML::ModelBuilder builder;
+ 
+    // Input spatial feature
     const int batchSize = 1;
     const int numSpatialFeatures = 22;
     const int nnXLen = 19;
     const int nnYLen = 19;
-
-    KataGoCoreML::ModelBuilder builder;
+ 
     KataGoCoreML::InputFeature inputSpatial("input_spatial",
                                             {batchSize, numSpatialFeatures, nnYLen, nnXLen});
 
     builder.addInputFeature(inputSpatial);
+
+    // Input global feature
+    const int numGlobalFeatures = 19;
+    KataGoCoreML::InputFeature inputGlobal("input_global", {1, numGlobalFeatures});
+    builder.addInputFeature(inputGlobal);
+
+    // Create a CoreML package with the model builder
     const std::string outputPath = "test_output.mlpackage";
     builder.createMLPackage(outputPath);
 
@@ -24,6 +33,6 @@ int main()
         return 1;
     }
 
-    std::cout << "✅ Successfully built minimal CoreML model at " << outputPath << std::endl;
+    std::cout << "✅ Successfully built a CoreML model at " << outputPath << std::endl;
     return 0;
 }
